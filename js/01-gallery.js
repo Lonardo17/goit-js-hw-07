5f126a75067dd2d88f53335c70f9b8190cdba4fa
Maksym Shumeiko <kkiilleerraa@mail.ru>
2022-05-27 21:34:05 +0300

    start

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+function createImg(el) {
+   const step = el.map(({ preview, original, description }) => {
+       return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}" >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    }).join("")
+    return step
+   }
+gallery.insertAdjacentHTML("afterbegin", createImg(galleryItems))
+gallery.addEventListener("click", selectedImg)
+
+function selectedImg(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") { return }
+    const dataAtr = event.target.dataset.source
+    // modal(dataAtr)
+    const massage = basicLightbox.create(`<img src="${dataAtr}">`)
+massage.show()
+}
+// function modal(dataAtr){
+// const massage = basicLightbox.create(`<img src="${dataAtr}">`)
+// gallery.onclick = massage.show
+// }
+
+// const instance = basicLightbox.create(content)
